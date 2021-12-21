423c7aeb0a957dc2a39e046d97cb7a4cfa1c9367
kukil-kashyap-borgohain <kukilp213@gmail.com>
2021-12-21 23:27:08 +0530

    Added daily practice problem

--- decToBin.py
+++ decToBin.py
@@ -1,7 +1,16 @@
+# Find number of 1s in a binary number.
+
 def decToBin(n):
-	if n >= 1:
-		decToBin(n // 2)
-	dec = n%2
-	print(dec)
+	# Convert to binary.
+	dec = bin(n).replace("0b","")
+	# Use comprehension to separate the integer to digits.
+	x = [int(i) for i in str(dec)]
+	print(x)
+	# Count number of 1's in the binary number.
+	count = 0
+	for i in range(0, len(x)):
+		if x[i] == 1:
+			count += 1
+	print("Number of 1's", count)
 
 decToBin(3)
---
+++ find-max-non-adjacent-sum.py
@@ -0,0 +1,4 @@
+# Find maximum non adjacent sum.
+# Example 1
+# l1 = [1,3,5,6,1,3]        max non adjacent sum = 9
+# l2 = [1,3,1,5,2,1,7,2]    max non adjacent sum = 12
